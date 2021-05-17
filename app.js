06b1f54e2f7e07385fcee6a9b910ac62ac725409
viruyu4yi <viter0k80@gmail.com>
2021-05-17 06:36:51 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv/config');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+//Import Routes
+const postsRoute = require('./routes/posts');
+
+app.use('/posts', postsRoute);
+
+//ROUTES
+app.get('/', (req, res) => {
+    res.send('We are on home');
+});
+
+//Connect to DB
+mongoose.connect(
+process.env.DB_CONNECTION, 
+{ useNewUrlParser: true ,
+ useUnifiedTopology: true },
+() => console.log('connected to DB!'),
+);
+
+//Listening server
+app.listen(3000);
